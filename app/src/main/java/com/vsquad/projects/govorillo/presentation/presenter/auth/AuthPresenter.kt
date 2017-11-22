4a44e307465b9e78b1003b0f79696761ad625be1
ShatalovM <shatalovmichael@gmail.com>
2017-11-23 02:33:18 +0300

    Auth update

--- app/src/main/java/com/vsquad/projects/govorillo/presentation/presenter/auth/AuthPresenter.kt
+++ app/src/main/java/com/vsquad/projects/govorillo/presentation/presenter/auth/AuthPresenter.kt
@@ -1,10 +1,7 @@
 package com.vsquad.projects.govorillo.presentation.presenter.auth
 
-import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import com.arellomobile.mvp.InjectViewState
-import com.arellomobile.mvp.MvpPresenter
 import com.google.android.gms.tasks.Task
 import com.vsquad.projects.govorillo.GovorilloApplication
 import com.vsquad.projects.govorillo.Screens
@@ -12,13 +9,6 @@ import com.vsquad.projects.govorillo.common.PrefConst
 import com.vsquad.projects.govorillo.common.SharedPrefUtils
 import com.vsquad.projects.govorillo.presentation.presenter.base.BaseFragmentPresenter
 import com.vsquad.projects.govorillo.presentation.view.auth.AuthView
-import android.support.annotation.NonNull
-import com.google.android.gms.tasks.OnCompleteListener
-import android.R.attr.password
-import android.support.v4.content.ContextCompat.startActivity
-import com.vsquad.projects.govorillo.ui.activity.main.MainActivity
-import android.content.Intent
-import android.R.attr.password
 import com.facebook.AccessToken
 import com.google.firebase.auth.*
 import com.vsquad.projects.govorillo.ui.fragment.auth.AuthFragment.Companion.TAG
@@ -88,5 +78,24 @@ class AuthPresenter : BaseFragmentPresenter<AuthView>() {
                 //Registration error
             }
         }
+    }
+
+    fun signInWithPhoneAuthCredential(token: PhoneAuthCredential){
+        Log.d(TAG, "signInWithPhoneAuthCredential");
+
+        //var credentials : AuthCredential = FacebookAuthProvider.getCredential(token.getToken());
+
+//        mAuth.signInWithCredential(credentials).addOnCompleteListener { task: Task<AuthResult> ->
+//            if (task.isSuccessful) {
+//                //Registration OK
+//                val firebaseUser = mAuth.currentUser
+//                SharedPrefUtils.edit().putBoolean(PrefConst.USER_AUTHORIZED, true)
+//                        .apply()
+//                updateUI(firebaseUser)
+//            } else {
+//                viewState.setErrorWhileRegistration()
+//                //Registration error
+//            }
+//        }
     }
 }
--- app/src/main/java/com/vsquad/projects/govorillo/ui/fragment/auth/AuthFragment.kt
+++ app/src/main/java/com/vsquad/projects/govorillo/ui/fragment/auth/AuthFragment.kt
@@ -9,24 +9,24 @@ import com.vsquad.projects.govorillo.R
 import com.vsquad.projects.govorillo.presentation.view.auth.AuthView
 import com.vsquad.projects.govorillo.presentation.presenter.auth.AuthPresenter
 
-import com.arellomobile.mvp.MvpAppCompatFragment
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.facebook.*
 import com.facebook.FacebookSdk.getApplicationContext
 import com.facebook.appevents.AppEventsLogger
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.vsquad.projects.govorillo.GovorilloApplication
-import com.vsquad.projects.govorillo.Screens
 import com.vsquad.projects.govorillo.ui.fragment.base.BaseFragment
 
 import kotlinx.android.synthetic.main.fragment_auth.*
-import com.vsquad.projects.govorillo.ui.fragment.auth.EmailValidator
-import kotlinx.android.synthetic.*
 import android.content.Intent
-
+import com.facebook.login.LoginManager
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.*
+import java.util.*
+import org.jetbrains.anko.*
+import java.util.concurrent.TimeUnit
+import com.google.firebase.auth.PhoneAuthProvider
 
 
 class AuthFragment : BaseFragment(), AuthView {
@@ -34,6 +34,32 @@ class AuthFragment : BaseFragment(), AuthView {
     private var mAuth: FirebaseAuth = FirebaseAuth.getInstance()
     var callbackManager: CallbackManager = CallbackManager.Factory.create()
     lateinit var facebookSignInButton: LoginButton
+    private lateinit var mResendToken : PhoneAuthProvider.ForceResendingToken
+    lateinit var mCallbacks : PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    /*
+    () {
+
+        override fun onVerificationCompleted(credential : PhoneAuthCredential) {
+            Log.d(TAG, "onVerificationCompleted:" + credential);
+            mAuthPresenter.signInWithPhoneAuthCredential(credential);
+        }
+
+
+        override fun onVerificationFailed(e : FirebaseException) {
+            Log.w(TAG, "onVerificationFailed", e);
+            println("Something went wrong")
+        }
+
+
+        override fun onCodeSent(verificationId : String, token : PhoneAuthProvider.ForceResendingToken) {
+            Log.d(TAG, "onCodeSent:" + verificationId);
+            mVerificationId = verificationId;
+            mResendToken = token;
+        }
+    }
+    */
+
+    lateinit var mVerificationId : String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         GovorilloApplication.INSTANCE.getAppComponent().inject(this)
@@ -77,17 +103,41 @@ class AuthFragment : BaseFragment(), AuthView {
                               savedInstanceState: Bundle?): View? {
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(getApplicationContext());
+
         return inflater.inflate(R.layout.fragment_auth, container, false)
 
     }
 
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+
+
+        override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         btn_sign_in.setOnClickListener {
-            if (EmailValidator().validate(et_email.text.toString())) {
-                mAuthPresenter.trySignIn(et_email.text.toString(), et_pass.text.toString())
-            }
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                    et_email.text.toString(),        // Phone number to verify
+                    60,                 // Timeout duration
+                    TimeUnit.SECONDS,   // Unit of timeout
+                    this,               // Activity (for callback binding)
+                    mCallbacks
+            )
+            activity.alert {
+                customView {
+                    verticalLayout {
+                        title("Подтверждение номера")
+                        val task = editText {
+                            hint = "Код из смс сообщения "
+                            padding = dip(20)
+                        }
+                        positiveButton("Подтвердить") {
+                            task.text.toString()        // OnVerificationStateChangedCallbacks
+                        }
+                    }
+                }
+            }.show()
+//            if (EmailValidator().validate(et_email.text.toString())) {
+//                mAuthPresenter.trySignIn(et_email.text.toString(), et_pass.text.toString())
+//            }
         }
         btn_sign_up.setOnClickListener {
             if (EmailValidator().validate(et_email.text.toString())) {
@@ -95,26 +145,32 @@ class AuthFragment : BaseFragment(), AuthView {
             }
         }
 
-        btn_sign_facebook.setReadPermissions("email")
-        btn_sign_facebook.fragment = this
-        callbackManager = CallbackManager.Factory.create()
-        btn_sign_facebook.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
-            override fun onSuccess(loginResult: LoginResult) {
-                // App code
-                Log.d("FACEBOOK_AUTH", "Success")
-                mAuthPresenter.signInWithFacebook(loginResult.accessToken);
-            }
+        btn_sign_facebook.setOnClickListener {
+            callbackManager = CallbackManager.Factory.create()
+            LoginManager
+                    .getInstance()
+                    .logInWithReadPermissions(
+                            this,
+                            Arrays.asList("public_profile", "user_friends", "email")
+                    )
+            LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                    // App code
+                    Log.d("FACEBOOK_AUTH", "Success")
+                    mAuthPresenter.signInWithFacebook(loginResult.accessToken);
+                }
 
-            override fun onCancel() {
-                // App code
-                Log.d("FACEBOOK_AUTH", "Cancel")
-            }
+                override fun onCancel() {
+                    // App code
+                    Log.d("FACEBOOK_AUTH", "Cancel")
+                }
 
-            override fun onError(exception: FacebookException) {
-                // App code
-                Log.d("FACEBOOK_AUTH", "Error")
-            }
-        })
+                override fun onError(exception: FacebookException) {
+                    // App code
+                    Log.d("FACEBOOK_AUTH", "Error")
+                }
+            })
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -122,3 +178,5 @@ class AuthFragment : BaseFragment(), AuthView {
         callbackManager.onActivityResult(requestCode, resultCode, data)
     }
 }
+
+private fun PhoneAuthProvider.verifyPhoneNumber(toString: String, i: Int, seconds: TimeUnit, authFragment: AuthFragment, onVerificationStateChangedCallbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks) {}
